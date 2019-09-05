46fe9a8fd27e4bd0e00c8c566e122c59e37e4808
Moritz Borcherding <moritz.borcherding@web.de>
2019-09-05 21:57:35 +0200

    more parts of the decoding parts in place ready to be implemented

--- src/decoding/mod.rs
+++ src/decoding/mod.rs
@@ -3,3 +3,4 @@ pub mod scratch;
 pub mod decodebuffer;
 pub mod fse;
 pub mod huff0;
+pub mod offset_history;
--- src/decoding/scratch.rs
+++ src/decoding/scratch.rs
@@ -1,16 +1,20 @@
 use std::collections::HashMap;
 use super::decodebuffer::Decodebuffer;
+use super::offset_history::OffsetHist;
 
 pub struct DecoderScratch {
    huf: HuffmanScratch,
    fse: FSEScratch,
    buffer: Decodebuffer,
+   offset_hist: OffsetHist,
 }
 
 pub struct HuffmanScratch {
-   pub decoding_map: HashMap<u32, u32>
+   pub decoding_map: HashMap<u32, u32>,
 }
 
 pub struct FSEScratch {
-   pub decoding_map: HashMap<u32, u32>
+   pub offset_decoding_map: HashMap<u32, u32>,
+   pub literal_length_decoding_map: HashMap<u32, u32>,
+   pub match_length_decoding_map: HashMap<u32, u32>,
 }
