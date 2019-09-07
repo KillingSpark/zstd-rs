a434566a49454e60e91f485e4997faf0002ccd82
Moritz Borcherding <moritz.borcherding@web.de>
2019-09-07 15:56:11 +0200

    renamed build_probabilities

--- src/decoding/fse.rs
+++ src/decoding/fse.rs
@@ -8,6 +8,7 @@ pub struct FSEDecoder {
     symbol_probablilities: Vec<i32>, //used while building the decode Vector
 }
 
+#[derive(Copy, Clone)]
 struct Entry {
     base_line: usize,
     num_bits: u8,
@@ -29,7 +30,7 @@ impl FSEDecoder {
     pub fn new() -> FSEDecoder {
         FSEDecoder {
             symbol_probablilities: Vec::with_capacity(256), //will never be more than 256 symbols because u8
-            decode: Vec::new(), //depending on acc_log. 
+            decode: Vec::new(),                             //depending on acc_log.
             accuracy_log: 0,
             state: 0,
         }
@@ -37,12 +38,10 @@ impl FSEDecoder {
 
     //returns how many BYTEs (not bits) were read while building the decoder
     pub fn build_decoder(&mut self, source: &[u8]) -> Result<usize, String> {
-        self.decode.clear(); //maybe truncate to save memory, but should be ok to just leave it to avoid alloctions in the furture
-        self.symbol_probablilities.clear(); //just clear, we will fill a probability for each entry anyways. Non eed to force new allocs here
         self.state = 0;
         self.accuracy_log = 0;
 
-        let bytes_read = self.build_probabilities(source)?;
+        let bytes_read = self.read_probabilities(source)?;
         self.build_decoding_table();
 
         Ok(bytes_read)
@@ -50,9 +49,67 @@ impl FSEDecoder {
 
     fn build_decoding_table(&mut self) {
         //TODO build decoding table
+        self.decode.clear();
+        let table_size = 1 << self.accuracy_log;
+        if self.decode.len() < table_size {
+            self.decode.reserve(table_size as usize - self.decode.len());
+        }
+        //fill with dummy entries
+        self.decode.resize(
+            table_size,
+            Entry {
+                base_line: 0,
+                num_bits: 0,
+                symbol: 0,
+            },
+        );
+
+        let mut negative_idx = table_size; //will point to the highest index with is already occupied by a negative-probability-symbol
+                                           
+        //first scan for all -1 probabilities and place them at the top of the table
+        for symbol in 0..self.symbol_probablilities.len() {
+            if self.symbol_probablilities[symbol] == -1 {
+                negative_idx -= 1;
+                let entry = &mut self.decode[negative_idx];
+                entry.symbol = symbol as u8;
+                entry.base_line = 0;
+                entry.num_bits = self.accuracy_log;
+            }
+        }
+
+        //then place in a semi-random order all of the other symbols
+        let mut position = 0;
+        for idx in 0..self.symbol_probablilities.len() {
+            let symbol = idx as u8;
+            if self.symbol_probablilities[idx] <= 0 {
+                continue;
+            }
+
+            //for each probability point the symbol gets on slot
+            let prob = self.symbol_probablilities[idx];
+            for state_number_for_symbol in 0..prob {
+                while position < negative_idx {
+                    //everything above negative_idx is already taken
+                    position = next_position(position, table_size);
+                }
+                let entry = &mut self.decode[negative_idx];
+                entry.symbol = symbol as u8;
+
+                let (bl, nb) = calc_baseline_and_numbits(
+                    table_size as u32,
+                    prob as u32,
+                    state_number_for_symbol as u32,
+                );
+
+                entry.base_line = bl;
+                entry.num_bits = nb;
+            }
+        }
     }
 
-    fn build_probabilities(&mut self, source: &[u8]) -> Result<usize, String> {
+    fn read_probabilities(&mut self, source: &[u8]) -> Result<usize, String> {
+        self.symbol_probablilities.clear(); //just clear, we will fill a probability for each entry anyways. Non eed to force new allocs here
+
         let mut bits_read = 0; //keep track of all bits read
 
         let mut br = BitReader::new();
@@ -95,7 +152,7 @@ impl FSEDecoder {
             if prob != 0 {
                 if prob > 0 {
                     probability_counter += prob as u32;
-                }else{
+                } else {
                     // probability -1 counts as 1
                     assert!(prob == -1);
                     probability_counter += 1;
@@ -124,5 +181,38 @@ impl FSEDecoder {
             bits_read / 8 + 1
         };
         Ok(bytes_read as usize)
+    }
+}
+
+
+//utility functions for building the decoding table from probabilities
+fn next_position(mut p: usize, table_size: usize) -> usize {
+    p += (table_size >> 1) + (table_size >> 3) + 3;
+    p &= table_size - 1;
+    p
+}
+
+fn calc_baseline_and_numbits(
+    num_states_total: u32,
+    num_states_symbol: u32,
+    state_number: u32,
+) -> (usize, u8) {
+    let num_state_slices = 1 << highest_bit_set(num_states_symbol); //always power of two
+
+    let num_double_width_state_slices = num_state_slices - num_states_symbol; //leftovers to the powerof two need to be distributed
+    let num_single_width_state_slices = num_state_slices - num_double_width_state_slices; //these will not receive a double width slice of states
+    let slice_width = num_states_total / num_state_slices; //size of a single width slice of states
+    let num_bits = highest_bit_set(slice_width); //number of bits needed to read for one slice
+
+    if state_number < num_double_width_state_slices {
+        //all single width
+        let baseline = num_single_width_state_slices * slice_width + state_number * slice_width * 2;
+        (baseline as usize, (num_bits + 1) as u8)
+    } else {
+        let index_in_single_width = state_number - num_double_width_state_slices;
+        (
+            (slice_width * index_in_single_width) as usize,
+            num_bits as u8,
+        )
     }
 }
