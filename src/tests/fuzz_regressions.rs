80440f440de11a2b264f003911c9a2d2bc30f4d1
Moritz Borcherding <moritz.borcherding@web.de>
2019-09-16 15:23:06 +0200

    filter artifacts for crashes. ignore slow units

--- src/tests/fuzz_regressions.rs
+++ src/tests/fuzz_regressions.rs
@@ -6,6 +6,12 @@ fn test_all_artifacts() {
 
     for file in fs::read_dir("./fuzz/artifacts/fuzz_target_1").unwrap() {
         let file_name = file.unwrap().path();
+        
+        let fnstr = file_name.to_str().unwrap().to_owned();
+        if !fnstr.contains("/crash-") {
+            continue
+        }
+
         let mut f = File::open(file_name.clone()).unwrap();
         match frame_decoder::FrameDecoder::new(&mut f) {
             Ok(mut frame_dec) => {
