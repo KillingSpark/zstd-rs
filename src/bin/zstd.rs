bb2e2a84abe34c25df9278ada7904be3b0a9c4a6
Moritz Borcherding <moritz.borcherding@web.de>
2019-09-24 22:51:08 +0200

    cooler output with only one self-updating line showing the percentages

--- src/bin/zstd.rs
+++ src/bin/zstd.rs
@@ -1,6 +1,7 @@
 extern crate zstd_rs;
 use std::fs::File;
 use std::io::Read;
+use std::io::Write;
 
 struct StateTracker {
     bytes_used: u64,
@@ -18,12 +19,12 @@ fn main() {
     let mut frame_dec = zstd_rs::FrameDecoder::new();
 
     for path in file_paths {
-        println!("File: {}", path);
+        std::io::stderr().write_fmt(format_args!("File: {}\n", path)).unwrap();
         let mut f = File::open(path).unwrap();
 
         frame_dec.reset(&mut f).unwrap();
 
-        let batch_size = 1024 * 1024;
+        let batch_size = 1024 * 1024 * 10;
         let mut result = Vec::with_capacity(batch_size);
         result.resize(batch_size, 0);
 
@@ -44,12 +45,14 @@ fn main() {
 
                 let percentage = (tracker.bytes_used * 100) / frame_dec.content_size().unwrap();
                 if percentage as i8 != tracker.old_percentage {
-                    //println!("{}", percentage);
+                    std::io::stderr().write_fmt(format_args!("\r")).unwrap();
+                    std::io::stderr().write_fmt(format_args!("{}", percentage)).unwrap();
                     tracker.old_percentage = percentage as i8;
                 }
             }
         }
-        println!("");
+
+        // handle the last chunk of data
         while frame_dec.can_collect() > 0 {
             let x = frame_dec.read(result.as_mut_slice()).unwrap();
 
@@ -58,15 +61,12 @@ fn main() {
             result.resize(result.capacity(), 0);
         }
 
-        // handle the last chunk of data
-        do_something(&mut result, &mut tracker);
-        println!("Decoded bytes: {}", tracker.bytes_used);
+        std::io::stderr().write_fmt(format_args!("\nDecoded bytes: {}\n", tracker.bytes_used)).unwrap();
     }
 }
 
 fn do_something(data: &Vec<u8>, s: &mut StateTracker) {
     //Do something. Like writing it to a file or to stdout...
-    use std::io::Write;
     std::io::stdout().write_all(data).unwrap();
     s.bytes_used += data.len() as u64;
 }
