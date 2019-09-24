e1b7241ceeefbc021bc649e112fe4e86cb92499d
Moritz Borcherding <moritz.borcherding@web.de>
2019-09-24 12:10:42 +0200

    added support to use decode_from_to() for draining as well

--- src/frame_decoder.rs
+++ src/frame_decoder.rs
@@ -260,82 +260,86 @@ impl FrameDecoder {
 
     /// Decodes as many blocks as possible from the source slice and reads from the decodebuffer into the target slice
     /// The source slice may contain only parts of a frame but must contain at least one full block to make progress
-    /// Returns (read, written), if read == 0 then the source did not contain a full block and further calls with the same 
-    /// input will not make any progress! 
-    /// 
-    /// Note that no kind of block can be bigger than 128kb. 
+    /// Returns (read, written), if read == 0 then the source did not contain a full block and further calls with the same
+    /// input will not make any progress!
+    ///
+    /// Note that no kind of block can be bigger than 128kb.
     /// So to be safe use at least 128*1024 (max block content size) + 3 (block_header size) + 18 (max frame_header size) bytes as your source buffer
+    ///
+    /// You may call this function with an empty source after all bytes have been decoded. This is equivalent to just call decoder.read(&mut traget)
     pub fn decode_from_to(
         &mut self,
         source: &[u8],
         target: &mut [u8],
     ) -> Result<(usize, usize), crate::errors::FrameDecoderError> {
-        let mut mt_source = &source[..];
-
         let bytes_read_at_start = match &mut self.state {
             Some(s) => s.bytes_read_counter,
             None => 0,
         };
 
-        if self.state.is_none() {
-            match self.init(&mut mt_source) {
-                Ok(()) => {}
-                Err(m) => return Err(crate::errors::FrameDecoderError::FailedToInitialize(m)),
+        if !self.is_finished() || self.state.is_none() {
+            let mut mt_source = &source[..];
+
+            if self.state.is_none() {
+                match self.init(&mut mt_source) {
+                    Ok(()) => {}
+                    Err(m) => return Err(crate::errors::FrameDecoderError::FailedToInitialize(m)),
+                }
             }
-        }
 
-        //pseudo block to scope "state" so we can borrow self again after the block
-        {
-            let mut state = match &mut self.state {
-                Some(s) => s,
-                None => panic!("Bug in library"),
-            };
-            let mut block_dec = decoding::block_decoder::new();
-
-            loop {
-                println!("{}", state.bytes_read_counter);
-                //check if there are enough bytes for the next header
-                if mt_source.len() < 3 {
-                    break;
-                }
-                let (block_header, block_header_size) = match block_dec
-                    .read_block_header(&mut mt_source)
-                {
-                    Ok(h) => h,
-                    Err(m) => {
-                        return Err(crate::errors::FrameDecoderError::FailedToReadBlockHeader(m))
-                    }
+            //pseudo block to scope "state" so we can borrow self again after the block
+            {
+                let mut state = match &mut self.state {
+                    Some(s) => s,
+                    None => panic!("Bug in library"),
                 };
+                let mut block_dec = decoding::block_decoder::new();
 
-                // check the needed size for the block before updating counters.
-                // If not enough bytes are in the source, the header will have to be read again, so act like we never read it in the first place
-                if mt_source.len() < block_header.content_size as usize {
-                    break;
-                }
-                state.bytes_read_counter += block_header_size as u64;
-
-                let bytes_read_in_block_body = match block_dec.decode_block_content(
-                    &block_header,
-                    &mut state.decoder_scratch,
-                    &mut mt_source,
-                ) {
-                    Ok(h) => h,
-                    Err(m) => {
-                        return Err(crate::errors::FrameDecoderError::FailedToReadBlockBody(m))
+                loop {
+                    //check if there are enough bytes for the next header
+                    if mt_source.len() < 3 {
+                        break;
                     }
-                };
-                state.bytes_read_counter += bytes_read_in_block_body;
-                state.block_counter += 1;
+                    let (block_header, block_header_size) =
+                        match block_dec.read_block_header(&mut mt_source) {
+                            Ok(h) => h,
+                            Err(m) => {
+                                return Err(
+                                    crate::errors::FrameDecoderError::FailedToReadBlockHeader(m),
+                                )
+                            }
+                        };
 
-                if block_header.last_block {
-                    state.frame_finished = true;
-                    if state.frame.header.descriptor.content_checksum_flag() {
-                        let chksum = &mt_source[..3];
-                        state.bytes_read_counter += 4;
-                        let _ = chksum;
-                        //TODO checksum
+                    // check the needed size for the block before updating counters.
+                    // If not enough bytes are in the source, the header will have to be read again, so act like we never read it in the first place
+                    if mt_source.len() < block_header.content_size as usize {
+                        break;
                     }
-                    break;
+                    state.bytes_read_counter += block_header_size as u64;
+
+                    let bytes_read_in_block_body = match block_dec.decode_block_content(
+                        &block_header,
+                        &mut state.decoder_scratch,
+                        &mut mt_source,
+                    ) {
+                        Ok(h) => h,
+                        Err(m) => {
+                            return Err(crate::errors::FrameDecoderError::FailedToReadBlockBody(m))
+                        }
+                    };
+                    state.bytes_read_counter += bytes_read_in_block_body;
+                    state.block_counter += 1;
+
+                    if block_header.last_block {
+                        state.frame_finished = true;
+                        if state.frame.header.descriptor.content_checksum_flag() {
+                            let chksum = &mt_source[..3];
+                            state.bytes_read_counter += 4;
+                            let _ = chksum;
+                            //TODO checksum
+                        }
+                        break;
+                    }
                 }
             }
         }
