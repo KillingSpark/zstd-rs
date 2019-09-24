df1525576cd3fc2baf018aa0a9a6999ec858cee4
Moritz Borcherding <moritz.borcherding@web.de>
2019-09-24 18:53:05 +0200

    added todo with further optimizations

--- src/decoding/decodebuffer.rs
+++ src/decoding/decodebuffer.rs
@@ -76,6 +76,8 @@ impl Decodebuffer {
                 self.buffer.push(self.buffer[start_idx + x]);
             }
         } else {
+            // TODO figure out way to do this in-place without the buffer. might need unsafe here. Would reduce the amount of coping by x2 
+            // so it would probably be worth it
             const BATCH_SIZE: usize = 32;
             let full_copies = match_length / BATCH_SIZE;
             let partial_copy_size = match_length % BATCH_SIZE;
