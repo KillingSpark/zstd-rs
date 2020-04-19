04a7a02f600c3c2e7f0f5722d764ec7c921dacf6
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2020-04-19 04:48:52 +0200

    Add a fuzzing target to verify that we can decompress any data compressed by the reference implementation

---
+++ fuzz/fuzz_targets/interop.rs
@@ -0,0 +1,22 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate ruzstd;
+use std::io::Read;
+
+fn decode_ruzstd(data: &mut dyn std::io::Read) -> Vec<u8> {
+    let mut decoder = ruzstd::StreamingDecoder::new(data).unwrap();
+    let mut result: Vec<u8> = Vec::new();
+    decoder.read_to_end(&mut result).expect("Decoding failed");
+    result
+}
+
+fn encode_zstd(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
+    zstd::stream::encode_all(std::io::Cursor::new(data), 3)
+}
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(compressed) = encode_zstd(data) {
+        let decoded = decode_ruzstd(&mut compressed.as_slice());
+        assert!(decoded == data, "Decoded data did not match the original input");
+    }
+});
