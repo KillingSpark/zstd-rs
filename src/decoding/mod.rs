eade38df3f93bb08ca2dd5e5ff2e9ea62d1da129
Paolo Barbolini <paolo.barbolini@m4ss.net>
2022-04-22 12:48:43 +0200

    fmt

--- src/decoding/mod.rs
+++ src/decoding/mod.rs
@@ -4,8 +4,8 @@ pub mod block_decoder;
 pub mod decodebuffer;
 pub mod dictionary;
 pub mod literals_section_decoder;
+mod ringbuffer;
 #[allow(dead_code)]
 pub mod scratch;
 pub mod sequence_execution;
 pub mod sequence_section_decoder;
-mod ringbuffer;
