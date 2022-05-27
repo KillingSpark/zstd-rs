54426b049fac90aec3e3724d918cb394d6f48854
Moritz Borcherding <moritz.borcherding@web.de>
2022-05-27 17:00:38 +0200

    cargo fmt

--- src/decoding/ringbuffer.rs
+++ src/decoding/ringbuffer.rs
@@ -56,7 +56,8 @@ impl RingBuffer {
             debug_assert!(usize::BITS >= 64 || new_cap < isize::MAX as usize);
         }
 
-        let new_layout = Layout::array::<u8>(new_cap).unwrap_or_else(|_| panic!("Could not create layout for u8 array of size {}", new_cap));
+        let new_layout = Layout::array::<u8>(new_cap)
+            .unwrap_or_else(|_| panic!("Could not create layout for u8 array of size {}", new_cap));
 
         // alloc the new memory region and panic if alloc fails
         // TODO maybe rework this to generate an error?
