d7bf7bfb7ef3f0ebd318ea131f099f2e9dabba87
zleyyij <75810274+zleyyij@users.noreply.github.com>
2024-08-12 09:01:35 -0600

    feat: rudimentary block header

--- src/encoding/block_header.rs
+++ src/encoding/block_header.rs
@@ -61,9 +61,12 @@ impl BlockHeader {
         // bw.write_bits(&[encoded_block_type], 2);
         // bw.write_bits(&self.block_size.to_le_bytes(), 21);
         // Ok(bw.dump().unwrap())
-        let mut block_header = self.block_size;
-        block_header |= (self.last_block as u32) << 31;
-        block_header |= encoded_block_type << 29;
+        // let mut block_header = self.block_size;
+        // block_header |= (self.last_block as u32) << 31;
+        // block_header |= encoded_block_type << 29;
+        let mut block_header = self.block_size << 3;
+        block_header |= encoded_block_type << 1;
+        block_header |= (self.last_block as u32);
         Ok(block_header.to_le_bytes()[0..3].to_vec())
     }
 }
@@ -72,41 +75,24 @@ impl BlockHeader {
 mod tests {
     use super::BlockHeader;
     use crate::{blocks::block::BlockType, decoding::block_decoder};
-    use std::string::String;
-    use std::{format, println};
 
     #[test]
     fn block_header_serialize() {
-        // let header = BlockHeader {
-        //     last_block: false,
-        //     block_type: super::BlockType::Raw,
-        //     block_size: 0b0000_0000_0001_1111_1111_1111_1111_1111,
-        // };
-        // println!("Number of 1s in block_size: {}", header.block_size.count_ones());
-        // let serialized_header = header.serialize().unwrap();
-        // let mut display_str = String::new();
-        // for byte in serialized_header.iter() {
-        //     display_str += &format!(" {byte:08b}");
-        // }
-        // // println!("BE: {display_str}");
-        // // println!(
-        // //     "LE: {}",
-        // //     display_str
-        // //         .split(" ")
-        // //         .collect::<std::vec::Vec<&str>>()
-        // //         .into_iter()
-        // //         .rev()
-        // //         .collect::<std::vec::Vec<&str>>()
-        // //         .join(" ")
-        // // );
-        // let mut decoder = block_decoder::new();
-        // let parsed_header = decoder
-        //     .read_block_header(serialized_header.as_slice())
-        //     .unwrap()
-        //     .0;
-        // assert!(parsed_header.last_block);
-        // assert_eq!(parsed_header.block_type, BlockType::Compressed);
-        // assert_eq!(parsed_header.content_size, 69);
+        let header = BlockHeader {
+            last_block: true,
+            block_type: super::BlockType::Compressed,
+            block_size: 69,
+        };
+        
+        let serialized_header = header.serialize().unwrap();
+        let mut decoder = block_decoder::new();
+        let parsed_header = decoder
+            .read_block_header(serialized_header.as_slice())
+            .unwrap()
+            .0;
 
+        assert!(parsed_header.last_block);
+        assert_eq!(parsed_header.block_type, BlockType::Compressed);
+        assert_eq!(parsed_header.content_size, 69);
     }
 }
