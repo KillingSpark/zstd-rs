a0d2031edb721a5da93c24a41586327e40e19ef9
Moritz Borcherding <moritz.borcherding@web.de>
2024-10-08 18:53:15 +0200

    build huffman table according to spec

--- src/huff0/huff0_encoder.rs
+++ src/huff0/huff0_encoder.rs
@@ -1,4 +1,5 @@
 use core::cmp::Ordering;
+use std::eprintln;
 
 use alloc::collections::VecDeque;
 use alloc::vec::Vec;
@@ -9,7 +10,7 @@ pub struct HuffmanTable {
 }
 
 impl HuffmanTable {
-    pub fn build(weights: &[usize]) -> Self {
+    pub fn build(weights: &[usize], maxNumBits: usize) -> Self {
         let mut sorted = Vec::with_capacity(weights.len());
         struct SortEntry {
             symbol: u8,
@@ -28,98 +29,46 @@ impl HuffmanTable {
             other => other,
         });
 
-        let mut queue1 = VecDeque::new();
-        let mut queue2 = VecDeque::new();
-        let mut nodes = Vec::new();
-
-        enum Node {
-            Leaf {
-                entry: SortEntry,
-            },
-            Internal {
-                left: usize,
-                right: usize,
-                weight: usize,
-            },
-        }
-        impl PartialEq for Node {
-            fn eq(&self, other: &Self) -> bool {
-                self.weight().eq(&other.weight())
-            }
-        }
-        impl Eq for Node {}
-        impl PartialOrd for Node {
-            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-                Some(self.weight().cmp(&other.weight()))
-            }
-        }
-        impl Ord for Node {
-            fn cmp(&self, other: &Self) -> Ordering {
-                self.weight().cmp(&other.weight())
-            }
-        }
-        impl Node {
-            fn weight(&self) -> usize {
-                match self {
-                    Self::Leaf { entry } => entry.weight,
-                    Self::Internal { weight, .. } => *weight,
-                }
-            }
-        }
-        for (idx, entry) in sorted.into_iter().enumerate() {
-            nodes.push(Node::Leaf { entry });
-            queue1.push_back(idx);
-        }
-
-        loop {
-            let min1 = pop_min(&mut queue1, &mut queue2, &nodes);
-            let min2 = pop_min(&mut queue1, &mut queue2, &nodes);
-            match (min1, min2) {
-                (Some(_root), None) | (None, Some(_root)) => {
-                    break;
-                }
-                (None, None) => unreachable!(),
-                (Some(left), Some(right)) => {
-                    nodes.push(Node::Internal {
-                        left,
-                        right,
-                        weight: nodes[left].weight() + nodes[right].weight(),
-                    });
-                    queue2.push_back(nodes.len() - 1);
-                }
-            }
-        }
         let mut table = HuffmanTable {
             codes: Vec::with_capacity(weights.len()),
         };
-        for _ in weights {
+        for _ in 0..weights.len() {
             table.codes.push((0, 0));
         }
 
-        // Todo traverse tree and put codes into the table
+        let mut currentWeight = sorted.last().unwrap().weight;
+        let mut currentNumBits = maxNumBits + 1 - currentWeight;
+        let mut code = (1 << currentNumBits) - 1;
+        for idx in (0..sorted.len()).rev() {
+            if currentWeight != sorted[idx].weight {
+                currentWeight = sorted[idx].weight;
+                let nextNumBits = maxNumBits + 1 - currentWeight;
+                code = (1 << (nextNumBits - currentNumBits)) - 1;
+                currentNumBits = nextNumBits;
+            }
+            eprintln!("Symbol: {}", sorted[idx].symbol);
+            table.codes[sorted[idx].symbol as usize] = (code, currentNumBits as u8);
+            code = code.saturating_sub(1);
+        }
 
         table
     }
 }
 
-fn pop_min<T: Ord>(
-    q1: &mut VecDeque<usize>,
-    q2: &mut VecDeque<usize>,
-    elements: &[T],
-) -> Option<usize> {
-    if q1.is_empty() {
-        q2.pop_front()
-    } else if q2.is_empty() {
-        q1.pop_front()
-    } else {
-        let e1 = &elements[*q1.front().unwrap()];
-        let e2 = &elements[*q2.front().unwrap()];
+#[test]
+fn huffman() {
+    let table = HuffmanTable::build(&[2, 2, 2, 1, 1], 3);
+    assert_eq!(table.codes[0], (1, 2));
+    assert_eq!(table.codes[1], (2, 2));
+    assert_eq!(table.codes[2], (3, 2));
+    assert_eq!(table.codes[3], (0, 3));
+    assert_eq!(table.codes[4], (1, 3));
 
-        match e1.cmp(e2) {
-            Ordering::Equal => q1,
-            Ordering::Less => q1,
-            Ordering::Greater => q2,
-        }
-        .pop_front()
-    }
+    let table = HuffmanTable::build(&[4, 3, 2, 0, 1, 1], 4);
+    assert_eq!(table.codes[0], (1, 1));
+    assert_eq!(table.codes[1], (1, 2));
+    assert_eq!(table.codes[2], (1, 3));
+    assert_eq!(table.codes[3], (0, 0));
+    assert_eq!(table.codes[4], (0, 4));
+    assert_eq!(table.codes[5], (1, 4));
 }
