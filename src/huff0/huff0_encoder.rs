b7dff63548dcc54a16373dfe7a3d7852b202d8b4
Moritz Borcherding <moritz.borcherding@web.de>
2024-10-08 19:20:37 +0200

    calc max num bits from weights

--- src/huff0/huff0_encoder.rs
+++ src/huff0/huff0_encoder.rs
@@ -1,7 +1,4 @@
 use core::cmp::Ordering;
-use std::eprintln;
-
-use alloc::collections::VecDeque;
 use alloc::vec::Vec;
 
 pub struct HuffmanTable {
@@ -10,7 +7,7 @@ pub struct HuffmanTable {
 }
 
 impl HuffmanTable {
-    pub fn build(weights: &[usize], maxNumBits: usize) -> Self {
+    pub fn build(weights: &[usize]) -> Self {
         let mut sorted = Vec::with_capacity(weights.len());
         struct SortEntry {
             symbol: u8,
@@ -36,18 +33,23 @@ impl HuffmanTable {
             table.codes.push((0, 0));
         }
 
-        let mut currentWeight = sorted.last().unwrap().weight;
-        let mut currentNumBits = maxNumBits + 1 - currentWeight;
-        let mut code = (1 << currentNumBits) - 1;
+        let weight_sum = sorted.iter().map(|e| 1 << (e.weight - 1)).sum::<usize>();
+        if !weight_sum.is_power_of_two() {
+            panic!("This is an internal error");
+        }
+        let max_num_bits = highest_bit_set(weight_sum) - 1; // this is a log_2 of a clean power of two
+
+        let mut current_weight = sorted.last().unwrap().weight;
+        let mut current_num_bits = max_num_bits + 1 - current_weight;
+        let mut code = (1 << current_num_bits) - 1;
         for idx in (0..sorted.len()).rev() {
-            if currentWeight != sorted[idx].weight {
-                currentWeight = sorted[idx].weight;
-                let nextNumBits = maxNumBits + 1 - currentWeight;
-                code = (1 << (nextNumBits - currentNumBits)) - 1;
-                currentNumBits = nextNumBits;
+            if current_weight != sorted[idx].weight {
+                current_weight = sorted[idx].weight;
+                let next_num_bits = max_num_bits + 1 - current_weight;
+                code = (1 << (next_num_bits - current_num_bits)) - 1;
+                current_num_bits = next_num_bits;
             }
-            eprintln!("Symbol: {}", sorted[idx].symbol);
-            table.codes[sorted[idx].symbol as usize] = (code, currentNumBits as u8);
+            table.codes[sorted[idx].symbol as usize] = (code, current_num_bits as u8);
             code = code.saturating_sub(1);
         }
 
@@ -55,16 +57,22 @@ impl HuffmanTable {
     }
 }
 
+/// Assert that the provided value is greater than zero, and returns index of the first set bit
+fn highest_bit_set(x: usize) -> usize {
+    assert!(x > 0);
+    usize::BITS as usize - x.leading_zeros() as usize
+}
+
 #[test]
 fn huffman() {
-    let table = HuffmanTable::build(&[2, 2, 2, 1, 1], 3);
+    let table = HuffmanTable::build(&[2, 2, 2, 1, 1]);
     assert_eq!(table.codes[0], (1, 2));
     assert_eq!(table.codes[1], (2, 2));
     assert_eq!(table.codes[2], (3, 2));
     assert_eq!(table.codes[3], (0, 3));
     assert_eq!(table.codes[4], (1, 3));
 
-    let table = HuffmanTable::build(&[4, 3, 2, 0, 1, 1], 4);
+    let table = HuffmanTable::build(&[4, 3, 2, 0, 1, 1]);
     assert_eq!(table.codes[0], (1, 1));
     assert_eq!(table.codes[1], (1, 2));
     assert_eq!(table.codes[2], (1, 3));
