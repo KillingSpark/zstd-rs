e175cd99c5d00954d49d7eea768082e0a58c0770
Moritz Borcherding <moritz.borcherding@web.de>
2024-11-17 15:05:36 +0100

    use hashmap to do find indexes of matches of a minimum length

--- src/encoding/match_generator.rs
+++ src/encoding/match_generator.rs
@@ -1,21 +1,25 @@
-use core::cmp::Ordering;
+use hashbrown::HashMap;
 
 use alloc::vec::Vec;
 
-struct SuffixIndex {
-    slice: usize,
-    idx: usize,
+const MIN_MATCH_LEN: usize = 5;
+
+struct WindowEntry<'data> {
+    data: &'data [u8],
+    suffixes: HashMap<[u8; MIN_MATCH_LEN], usize>,
+    base_offset: usize,
 }
 
 pub(crate) struct MatchGenerator<'data> {
     max_window_size: usize,
-    suffixes: Vec<SuffixIndex>,
     /// Data window we are operating on to find matches
     /// The data we want to find matches for is in the last slice
-    window: Vec<&'data [u8]>,
+    window: Vec<WindowEntry<'data>>,
     window_size: usize,
     /// Index in the last slice that we already processed
     suffix_idx: usize,
+    /// Gets updated when a new sequence is returned to point right behind that sequence
+    last_idx_in_sequence: usize,
 }
 
 #[derive(PartialEq, Eq, Debug)]
@@ -34,80 +38,114 @@ impl<'data> MatchGenerator<'data> {
     pub(crate) fn new(max_size: usize) -> Self {
         Self {
             max_window_size: max_size,
-            suffixes: Vec::new(),
             window: Vec::new(),
             window_size: 0,
             suffix_idx: 0,
+            last_idx_in_sequence: 0,
         }
     }
 
     pub(crate) fn next_sequence(&mut self) -> Option<Sequence<'data>> {
-        let data_slice = self.window.last().unwrap();
+        let last_entry = self.window.last().unwrap();
+        let data_slice = last_entry.data;
         if self.suffix_idx >= data_slice.len() {
             return None;
         }
+        let data_slice = &data_slice[self.suffix_idx..];
 
-        let suffix_idx = SuffixIndex {
-            slice: self.window.len() - 1,
-            idx: self.suffix_idx,
-        };
-        if self.suffixes.is_empty() {
-            assert!(self.window.len() == 1);
-            assert!(self.suffix_idx == 0);
-            self.suffixes.push(suffix_idx);
-
-            // TODO find a better way instead of just pushing the first block without any sequences
-            self.suffix_idx += data_slice.len();
+        if data_slice.len() < MIN_MATCH_LEN {
+            let last_idx_in_sequence = self.last_idx_in_sequence;
+            self.last_idx_in_sequence = last_entry.data.len();
+            self.suffix_idx = last_entry.data.len();
             return Some(Sequence::Literals {
-                literals: data_slice,
+                literals: &last_entry.data[last_idx_in_sequence..],
             });
         }
 
-        let suffix = &data_slice[self.suffix_idx..];
+        let mut key = [0u8; MIN_MATCH_LEN];
+        key.copy_from_slice(&data_slice[..MIN_MATCH_LEN]);
 
-        let mut upper_limit = self.suffixes.len() - 1;
-        let mut lower_limit = 0;
-        let mut candidate = Vec::with_capacity(self.window.len());
-        let insert_idx = loop {
-            let search_idx = (upper_limit + lower_limit) / 2;
-            let candidate_idx = &self.suffixes[search_idx];
-            candidate.clear();
-            candidate.extend_from_slice(&self.window[candidate_idx.slice..]);
-            candidate[0] = &candidate[0][candidate_idx.idx..];
-            let last_idx = candidate.len() - 1;
-            candidate[last_idx] = &candidate[last_idx][..self.suffix_idx];
+        let mut sequence = None;
 
-            match compare_suffix(suffix, candidate.as_slice()) {
-                Ordering::Equal => {
-                    break search_idx;
+        for (match_entry_idx, match_entry) in self.window.iter().enumerate() {
+            let is_last = match_entry_idx == self.window.len() - 1;
+            if let Some(match_index) = match_entry.suffixes.get(&key).copied() {
+                let match_slice = if is_last {
+                    &match_entry.data[match_index..self.suffix_idx]
+                } else {
+                    &match_entry.data[match_index..]
+                };
+                let min_len = usize::min(match_slice.len(), data_slice.len());
+
+                let mut match_len = 0;
+                for idx in 0..min_len {
+                    if match_slice[idx] != data_slice[idx] {
+                        break;
+                    }
+                    match_len = idx + 1;
                 }
-                Ordering::Less => {
-                    upper_limit = search_idx;
-                }
-                Ordering::Greater => {
-                    lower_limit = search_idx;
+
+                if match_len >= MIN_MATCH_LEN {
+                    let literals = &last_entry.data[self.last_idx_in_sequence..self.suffix_idx];
+                    let offset = match_entry.base_offset - match_index + self.suffix_idx;
+                    sequence = Some(Sequence::Triple {
+                        literals,
+                        offset,
+                        match_len,
+                    });
+
+                    break;
                 }
             }
+        }
 
-            if upper_limit - lower_limit <= 1 {
-                break search_idx;
+        if let Some(Sequence::Triple { match_len, .. }) = sequence {
+            self.add_suffixes_till(self.suffix_idx + match_len);
+            self.suffix_idx += match_len;
+            self.last_idx_in_sequence = self.suffix_idx;
+        } else {
+            let last_entry = self.window.last_mut().unwrap();
+            if !last_entry.suffixes.contains_key(&key) {
+                last_entry.suffixes.insert(key, self.suffix_idx);
             }
-        };
+            self.suffix_idx += 1;
+        }
 
-        self.suffixes.insert(insert_idx, suffix_idx);
-        // TODO find longest matches between the new neighbours
-        self.suffix_idx += data_slice.len();
-        return Some(Sequence::Literals {
-            literals: data_slice,
-        });
+        sequence
+    }
+
+    fn add_suffixes_till(&mut self, idx: usize) {
+        let last_entry = self.window.last_mut().unwrap();
+        let last_idx = usize::min(idx, last_entry.data.len() - MIN_MATCH_LEN);
+        for idx in self.suffix_idx..last_idx {
+            let mut key = [0u8; MIN_MATCH_LEN];
+            key.copy_from_slice(&last_entry.data[idx..idx + MIN_MATCH_LEN]);
+            if !last_entry.suffixes.contains_key(&key) {
+                last_entry.suffixes.insert(key, idx);
+            }
+        }
     }
 
     pub(crate) fn add_data(&mut self, data: &'data [u8]) {
-        assert!(self.window.is_empty() || self.suffix_idx == self.window.last().unwrap().len());
+        assert!(
+            self.window.is_empty() || self.suffix_idx == self.window.last().unwrap().data.len()
+        );
         self.reserve(data.len());
-        self.window.push(data);
+
+        if let Some(last_len) = self.window.last().map(|last| last.data.len()) {
+            for entry in self.window.iter_mut() {
+                entry.base_offset += last_len;
+            }
+        }
+
+        self.window.push(WindowEntry {
+            data,
+            suffixes: HashMap::with_capacity(data.len()),
+            base_offset: 0,
+        });
         self.window_size += data.len();
         self.suffix_idx = 0;
+        self.last_idx_in_sequence = 0;
     }
 
     fn reserve(&mut self, amount: usize) {
@@ -115,45 +153,87 @@ impl<'data> MatchGenerator<'data> {
         let mut removed_slices = 0;
         while self.window_size + amount > self.max_window_size {
             let removed = self.window.remove(0);
-            self.window_size -= removed.len();
+            self.window_size -= removed.data.len();
             removed_slices += 1;
         }
         if removed_slices == 0 {
             return;
         }
-        self.suffixes.retain_mut(|suffix_index| {
-            if suffix_index.slice < removed_slices {
-                false
-            } else {
-                suffix_index.slice -= removed_slices;
-                true
-            }
-        });
     }
-}
-
-fn compare_suffix(suffix: &[u8], window: &[&[u8]]) -> Ordering {
-    for (idx, b) in window.iter().flat_map(|slice| slice.iter()).enumerate() {
-        if idx > suffix.len() {
-            break;
-        }
-        let cmp = suffix[idx].cmp(b);
-        if cmp != Ordering::Equal {
-            return cmp;
-        }
-    }
-    Ordering::Equal
 }
 
 #[test]
 fn matches() {
-    let mut matcher = MatchGenerator::new(16);
-    matcher.add_data(&[0, 0, 0, 0]);
-    let seq = matcher.next_sequence().unwrap();
+    let mut matcher = MatchGenerator::new(1000);
+    matcher.add_data(&[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+    for _ in 0..5 {
+        assert!(matcher.next_sequence().is_none());
+    }
+
     assert_eq!(
-        seq,
-        Sequence::Literals {
-            literals: &[0, 0, 0, 0]
+        matcher.next_sequence().unwrap(),
+        Sequence::Triple {
+            literals: &[0, 0, 0, 0, 0],
+            offset: 5,
+            match_len: 5
         }
     );
+    assert!(matcher.next_sequence().is_none());
+
+    matcher.add_data(&[
+        1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0,
+    ]);
+
+    for _ in 0..6 {
+        assert!(matcher.next_sequence().is_none());
+    }
+    assert_eq!(
+        matcher.next_sequence().unwrap(),
+        Sequence::Triple {
+            literals: &[1, 2, 3, 4, 5, 6],
+            offset: 6,
+            match_len: 6
+        }
+    );
+    assert_eq!(
+        matcher.next_sequence().unwrap(),
+        Sequence::Triple {
+            literals: &[],
+            offset: 12,
+            match_len: 6
+        }
+    );
+    assert_eq!(
+        matcher.next_sequence().unwrap(),
+        Sequence::Triple {
+            literals: &[],
+            offset: 28  ,
+            match_len: 5
+        }
+    );
+    assert!(matcher.next_sequence().is_none());
+
+    matcher.add_data(&[
+        1, 2, 3, 4, 5, 6, 7, 0, 0, 0, 0, 0,
+    ]);
+
+    assert_eq!(
+        matcher.next_sequence().unwrap(),
+        Sequence::Triple {
+            literals: &[],
+            offset: 23,
+            match_len: 6
+        }
+    );
+    assert!(matcher.next_sequence().is_none());
+    assert_eq!(
+        matcher.next_sequence().unwrap(),
+        Sequence::Triple {
+            literals: &[7],
+            offset: 40,
+            match_len: 5
+        }
+    );
+    assert!(matcher.next_sequence().is_none());
 }
