6d5573b528a49adb81cd47ac427664a99b96e6d7
Paolo Barbolini <paolo.barbolini@m4ss.net>
2024-11-28 09:36:37 +0100

    Add debug assertions to RingBuffer::extend_from_within_unchecked

--- src/decoding/ringbuffer.rs
+++ src/decoding/ringbuffer.rs
@@ -295,6 +295,9 @@ impl RingBuffer {
     /// 2. More then len reserved space so we do not write out-of-bounds
     #[warn(unsafe_op_in_unsafe_fn)]
     pub unsafe fn extend_from_within_unchecked(&mut self, start: usize, len: usize) {
+        debug_assert!(start + len <= self.len());
+        debug_assert!(self.free() >= len);
+
         if self.head < self.tail {
             // Continuous source section and possibly non continuous write section:
             //
