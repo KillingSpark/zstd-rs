74a62d997d2bd42777e97da68addf12c0f8a9d2a
Moritz Borcherding <moritz.borcherding@web.de>
2025-11-02 12:29:31 +0100

    revert update of Readme Path in lib.rs

--- ruzstd/src/lib.rs
+++ ruzstd/src/lib.rs
@@ -10,7 +10,7 @@
 //! Compression can be achieved by using the [`encoding::compress`]/[`encoding::compress_to_vec`]
 //! functions or [`encoding::FrameCompressor`]
 //!
-#![doc = include_str!("../Readme.md")]
+#![doc = include_str!("../../Readme.md")]
 #![no_std]
 #![deny(trivial_casts, trivial_numeric_casts, rust_2018_idioms)]
 
